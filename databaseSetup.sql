bd738a637cda23015f6b649fa45bee25d0412e58
Alex Kolstad <ajkolstad@gmail.com>
2020-11-18 11:17:34 -0800

    Updated databaseSetup

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -1,17 +1,17 @@
 CREATE TABLE User
 (
-  Username VARCHAR(20) NOT NULL,
-  Password VARCHAR(20) NOT NULL,
-  Name VARCHAR(40) NOT NULL,
+  Username VARCHAR(255) NOT NULL,
+  Password VARCHAR(255) NOT NULL,
+  Name VARCHAR(255) NOT NULL,
   PRIMARY KEY (Username)
 );
 
 CREATE TABLE Target_Depth
 (
   Tdepth FLOAT NOT NULL,
-  Target_Flume_Name VARCHAR(20) NOT NULL,
+  Target_Flume_Name INT NOT NULL,
   Tdate DATE NOT NULL,
-  Username VARCHAR(20) NOT NULL,
+  Username VARCHAR(255) NOT NULL,
   PRIMARY KEY (Tdate),
   FOREIGN KEY (Username) REFERENCES User(Username)
 );
@@ -20,6 +20,6 @@ CREATE TABLE Depth_Data
 (
   Ddate DATE NOT NULL,
   Depth FLOAT NOT NULL,
-  Depth_Flume_Name VARCHAR(20) NOT NULL,
+  Depth_Flume_Name INT NOT NULL,
   PRIMARY KEY (Ddate)
 );
