e02df5c32424b9ebe78e86aeef9c936202764e3b
Alex Kolstad <ajkolstad@gmail.com>
2020-11-20 12:39:53 -0800

    Cleaned Up

--- Depth-Receiver/webscrapper.py
+++ Depth-Receiver/webscrapper.py
@@ -7,7 +7,6 @@ soup= BeautifulSoup(html_text, 'lxml')
 
 soupArray = soup.prettify().split('\n');
 
-
 line = soupArray[237]
 
 start = 0
@@ -16,20 +15,16 @@ end = 0
 for index, char in enumerate(reversed(line)):
     if char == ',':
         start = int(index)
-        print(start)
         break
-
 
 for index, char in enumerate(reversed(line)):
     if char in '0123456789':
         end = int(index)
-        print(end)
         break
 
 startInt = len(line) - start
 endInt = len(line) - end
 
-print(startInt)
-print(endInt)
+curDepth = line[startInt : endInt]
 
-print(line[startInt : endInt])
+print(curDepth)
