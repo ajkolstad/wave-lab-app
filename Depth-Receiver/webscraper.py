b0b9dab6373a990fd611000a095353214a4a3e88
TobiasBird <Birdto@oregonstate.edu>
2021-01-19 01:08:20 -0800

    Adding actual front end, not just app starting code

--- Depth-Receiver/webscraper.py
+++ Depth-Receiver/webscraper.py
@@ -5,7 +5,15 @@ import os
 from bs4 import BeautifulSoup
 from datetime import datetime
 from os.path import join, dirname
+from mysql.connector import Error
 from dotenv import load_dotenv
+
+# Grab information from config.env    
+dotenv_path = join(dirname(__file__), 'config.env')
+load_dotenv(dotenv_path) 
+
+# The sql call to insert the depth
+sql = """INSERT INTO depth_data(Ddate, Depth, Depth_Flume_Name) VALUES(%s, %s, %s)"""
 
 # Gets the depth of the flume that is called by str by web scraping
 def depth_flume(str):
@@ -45,24 +53,31 @@ def depth_flume(str):
 
     return curDepth
 
-def main():
+def insert_data(cur_data, depth_lwf, nflume):
 
-    # Grab information from config.env    
-    dotenv_path = join(dirname(__file__), 'config.env')
-    load_dotenv(dotenv_path)    
-    
-    # Connects to the database
-    conn = mysql.connector.connect(
-        host = os.getenv('DATABASE_HOST'),
-        user = os.getenv('DATABASE_USER'),
-        password = os.getenv('DATABASE_PASSWORD'),
-        database = os.getenv('DATABASE')
-    )
-    cursor = conn.cursor()
+    try:
+        # Connects to the database
+        conn = mysql.connector.connect(
+            host = os.getenv('DATABASE_HOST'),
+            user = os.getenv('DATABASE_USER'),
+            password = os.getenv('DATABASE_PASSWORD'),
+            database = os.getenv('DATABASE')
+        )
+        if conn.is_connected():
+            cursor = conn.cursor()
+            val = (cur_data, depth_lwf, nflume)
+            cursor.execute(sql, val)
+            conn.commit()
+    except Error as e:
+        print ("Error connecting to MySQL", e)
+    finally:
+        if(conn.is_connected()):
+            cursor.close()
+            conn.close()
 
-    # The sql call to insert the depth
-    sql = """INSERT INTO depth_data(Ddate, Depth, Depth_Flume_Name) VALUES(%s, %s, %s)"""
-    
+
+def main():   
+
     # Run infinitly
     while(True):
 
@@ -70,9 +85,7 @@ def main():
         depth_lwf = depth_flume("LWF")
         now = datetime.now()
         cur_date = now.strftime("%Y-%m-%d %H:%M:%S")
-        val = (cur_date, depth_lwf, 0)
-        cursor.execute(sql, val)
-        conn.commit()
+        insert_data(cur_date, depth_lwf, 0)
         
         # Sleep for 5 minutes
         time.sleep(300)
@@ -81,9 +94,7 @@ def main():
         depth_dwb = depth_flume("DWB")
         now = datetime.now()
         cur_date = now.strftime("%Y-%m-%d %H:%M:%S")
-        val = (cur_date, depth_dwb, 1)
-        cursor.execute(sql, val)
-        conn.commit()
+        insert_data(cur_date, depth_dwb, 1)
 
         # Sleep for 5 minutes
         time.sleep(300)
