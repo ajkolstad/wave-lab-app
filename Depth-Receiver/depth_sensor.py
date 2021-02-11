bccbd79c07dfa9998898e9ce0a9eb1fc975d6774
Trevor D Byko <38079674+bykot1@users.noreply.github.com>
2021-02-10 17:17:29 -0800

    Depth sensor connect to database

--- Depth-Receiver/depth_sensor.py
+++ Depth-Receiver/depth_sensor.py
@@ -1,47 +1,60 @@
 import sys, os, os.path
 import mysql.connector
+import sqlalchemy as db
 
 from datetime import datetime
-from sqlalchemy.orm import relation, backref
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (
+    Column,
+    Float,
+    Integer,
+    DateTime
     )
 
-DBSession = scoped_session(sessionmaker())
-Base = declarative_base()
+DEBUG = False
 
-# Depth data file path, sub directories DWB and LWF exist
+Base = declarative_base()
+class Data(Base):
+    __tablename__ = 'data'
+    id = Column(Integer, primary_key=True)
+    value = Column(Float, index=True)
+    basin = Column(Integer, index=True) # flume=0, basin=1
+    datetime = Column(DateTime, index=True)
+
 ARCHIVE = "/a1/walve/data"
 FACILITIES = [ "LWF", "DWB"]
 
 # The file path of the sqlite database
-DBFILEPATH = "/a1/walve/data/levels.sqlite"
-HOLD_DATA_FOR = 60 # days
-REDUCE_DATA_RESOLUTION_AFTER = 48 # hours 
-
+# DBFILEPATH = "/a1/walve/data/levels.sqlite"
+DBFILEPATH = "levels.sqlite"
 
 def main():
-    argv=sys.argv
-    if len(argv) != 2:
-        usage(argv)
-
-    fn = argv[1]
-    if os.path.exists(fn):
-        print("File exists: %s" % fn)
-    else:
-        print("File doesn't exist: %s" % fn)
-
-    engine = create_engine('sqlite:///%s' % fn)
-    print(engine.table_names)
-
-
-    DBSession.configure(bind=engine)
-    Base.metadata.create_all(engine)
+    path = DBFILEPATH
+    argv = sys.argv
     
-    print("successful connect")
+    if len(argv) == 2:
+        path = argv[1]
+    
+    if os.path.exists(path):
+        print("File exists: %s" % path)
+    else:
+        print("File doesn't exist: %s" % path)
+
+    # Create db connection and metadata
+    engine = db.create_engine('sqlite:///%s' % path, echo=DEBUG)
+    connection = engine.connect()
+    metaData = db.MetaData()
+
+    # Load data table
+    depths = db.Table('data', metaData, autoload=True, autoload_with=engine)
+    Session = sessionmaker(bind = engine)
+    session = Session()
+
+    results = session.query(Data).order_by(Data.datetime.desc()).limit(2)
+    for row in results:
+        print(row.basin, " : ", row.value)
+
 
 if __name__ == "__main__":
     main()
